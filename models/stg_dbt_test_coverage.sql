9c7edb2b5d2744c8a6dc771f6cf578b799d9f7e7
Michael Hartmann <mh@mihama.de>
2019-07-17 18:39:10 +0200

    Fix model syntax

--- models/stg_dbt_test_coverage.sql
+++ models/stg_dbt_test_coverage.sql
@@ -1,12 +1,12 @@
 SELECT
-    models.id,
-    models.schema,
-    models.name,
-    tests.created_at,
-    count(tests.id)
+    models.id as id,
+    models.schema as schema,
+    models.name as name,
+    tests.created_at as tested_at,
+    count(tests.id) as nr_tests
 
 FROM {{this.schema}}.dbt_tests tests
-LEFT JOIN {{ ref(stg_dbt_models) }} models
+LEFT JOIN {{ ref('stg_dbt_models') }} models
     ON tests.tested_model_id = models.id
 
 GROUP BY
