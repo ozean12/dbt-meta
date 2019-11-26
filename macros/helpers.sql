b826782f955eb987f5186a6038654d99860f36ee
Michael Hartmann <mh@mihama.de>
2019-11-26 10:49:01 +0100

    Correct equality test

--- macros/helpers.sql
+++ macros/helpers.sql
@@ -1,7 +1,7 @@
 {% macro log_run_start() %}
 
   -- Log only if executed by dbt user
-  {%- if target.schema = 'dbt_user' -%}
+  {%- if target.schema == 'dbt_user' -%}
 
       -- Create tables if not already existing
       {{ meta.create_logging_table() }}
@@ -20,7 +20,7 @@
 {% macro log_run_end() %}
 
   -- Log only if executed by dbt user
-  {%- if target.schema = 'dbt_user' -%}
+  {%- if target.schema == 'dbt_user' -%}
 
       -- Log end of execution
       {{ meta.log_run_end_event() }}
@@ -33,7 +33,7 @@
 {% macro log_model_start() %}
 
   -- Log only if executed by dbt user
-  {%- if target.schema = 'dbt_user' -%}
+  {%- if target.schema == 'dbt_user' -%}
 
       -- Log start of model build
       {{ meta.log_model_start_event() }}
@@ -46,7 +46,7 @@
 {% macro log_model_end() %}
 
   -- Log only if executed by dbt user
-  {%- if target.schema = 'dbt_user' -%}
+  {%- if target.schema == 'dbt_user' -%}
 
       -- Log start of model build
       {{ meta.log_model_end_event() }}
