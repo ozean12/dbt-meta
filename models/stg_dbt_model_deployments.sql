602b238058542bf8a51b7a8f0618efd770c9ffcd
Michael Hartmann <mh@mihama.de>
2020-06-24 17:21:01 +0200

    update usages of surrogate key to new syntax

--- models/stg_dbt_model_deployments.sql
+++ models/stg_dbt_model_deployments.sql
@@ -6,24 +6,24 @@ with events as (
 
 aggregated as (
 
-    select 
-    
+    select
+
         {{dbt_utils.surrogate_key(
-            'event_model', 
-            'invocation_id'
+            ['event_model',
+            'invocation_id']
             )}} as model_deployment_id,
-    
+
         invocation_id,
         event_model as model,
-    
-        min(case 
-            when event_name = 'model deployment started' then event_timestamp 
+
+        min(case
+            when event_name = 'model deployment started' then event_timestamp
             end) as deployment_started_at,
-    
-        min(case 
-            when event_name = 'model deployment completed' then event_timestamp 
+
+        min(case
+            when event_name = 'model deployment completed' then event_timestamp
             end) as deployment_completed_at
-    
+
     from events
     where event_name ilike '%model%'
     group by 1, 2, 3
